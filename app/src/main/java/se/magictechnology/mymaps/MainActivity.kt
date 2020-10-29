328f2f0028ac7931665e68b8ad6d8f491c043d8d
Bill Mårtensson <bill@magictechnology.se>
2020-10-29 21:16:23 +0100

    Start

---
+++ app/src/main/java/se/magictechnology/mymaps/MainActivity.kt
@@ -0,0 +1,142 @@
+package se.magictechnology.mymaps
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Camera
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    lateinit var locationManager: LocationManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.themap) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        getLocation()
+
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == 1)
+        {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                getLocation()
+            }
+        }
+
+    }
+
+    fun getLocation()
+    {
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this,arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,android.Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+            return
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0F, object : LocationListener {
+            override fun onLocationChanged(p0: Location?) {
+
+                Log.i("BILLDEBUG", p0!!.latitude.toString())
+
+                mMap.apply {
+                    val mypos = LatLng(p0!!.latitude, p0!!.longitude)
+
+                    addMarker(
+                        MarkerOptions()
+                            .position(mypos)
+                            .title("My position")
+                    )
+                }
+
+
+            }
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+            }
+
+            override fun onProviderEnabled(provider: String?) {
+
+            }
+
+            override fun onProviderDisabled(provider: String?) {
+
+            }
+
+        })
+    }
+
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+
+
+        mMap.apply {
+            val sydney = LatLng(-33.852, 151.211)
+
+            moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10F))
+
+            addMarker(
+                    MarkerOptions()
+                            .position(sydney)
+                            .title("Marker in Sydney")
+            )
+        }
+        mMap.setOnMarkerClickListener(object : GoogleMap.OnMarkerClickListener {
+            override fun onMarkerClick(marker: Marker): Boolean {
+
+                Log.i("BILLDEBUG", marker.title)
+
+                return false
+            }
+        })
+    }
+
+
+}
